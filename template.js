159fd9db74090e37d9e7bafb0d8624fe8fca3a8b
Mateo <matthew27garcia@gmail.com>
2021-09-21 22:38:48 -0500

    stuff

---
+++ template.js
@@ -0,0 +1,17 @@
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
+var thumbnail = document.getElementsByClassName("thumbnail");
+var hero = document.getElementsById(".hero");
+
+var backgroundImg = new Array(
+"images/earth.jpg");
+"images/valley.jpg";
+"images/bridge.jpg";
+"images/nightsky.jpg";
+"images/stars.jpg";
+
+let i = 0;
+next.onclick = function() {
+    'url("'+backgroundImg[i+1]+'")'
+    var heroTag =document.querySelector(".hero")
+}
